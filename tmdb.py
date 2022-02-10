9ac8238178d9b4d83fdfed2888eada2504fe1160
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-09 22:32:09 -0500

    created mediawiki API functionality using date and movie title info from get_movie_data()

--- tmdb.py
+++ tmdb.py
@@ -1,8 +1,8 @@
 """program to find new movies"""
 import os
+from re import search
 import requests
 from dotenv import load_dotenv, find_dotenv
-
 
 # will probably be using this to find movie information:
 # https://developers.themoviedb.org/3/find/find-by-id
@@ -14,7 +14,7 @@ load_dotenv(find_dotenv())
 def get_movie_data(external_id):
     """returns info about a movie"""
     base_url = "https://api.themoviedb.org/3/movie/" + external_id + \
-        "?api_key=e3e0ecf40bbeb7110f2d1a749fe12eba&language=en-US"
+        "?api_key="+os.getenv("TMDB_KEY")+"&language=en-US"
     response = requests.get(base_url)
     response_json = response.json()
     genre_ids = response_json['genres']
@@ -25,10 +25,19 @@ def get_movie_data(external_id):
     overview = response_json['overview']
     genre_names = ', '.join(genres)
     picture = "https://image.tmdb.org/t/p/w500" + response_json['poster_path']
-    return  [title, overview, genre_names, picture]
+    date = response_json['release_date'][0:4]
+    return  [title, overview, genre_names, picture, date]
 
-# base_url = "https://api.themoviedb.org/3/movie/" + "157336" + \
-#        "?api_key=e3e0ecf40bbeb7110f2d1a749fe12eba&language=en-US"
-# response = requests.get(base_url)
-# response_json = response.json()
-# print(response_json['poster_path'])
+def wiki_page(title, year):
+    """wikipedia api"""
+    movie = title + year
+    base_url = "https://en.wikipedia.org/w/api.php"
+    query_params = {
+        "action": "opensearch",
+        "search": movie,
+        "format": "json",
+    }
+
+    the_response = requests.get(base_url, params=query_params)
+    json_response = the_response.json()
+    return json_response[3][0]
