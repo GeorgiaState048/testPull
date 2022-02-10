eb3bca687d31ab07a4f965f0c1a20a38d5fdf046
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-10 01:47:19 -0500

    renamed main.py file and added MediaWiki API

---
+++ app.py
@@ -0,0 +1,23 @@
+"""app file"""
+import random
+import flask
+from tmdb_wiki import get_movie_data, wiki_page
+
+
+app = flask.Flask(__name__)
+"""routes interpret different pages of a page"""
+
+@app.route("/")
+def index():
+    """home page"""
+    external_ids = ["558", "557", "419430", "496243"]
+    external_id = random.choice(external_ids)
+    movie_data = get_movie_data(external_id)
+    wiki_link = wiki_page(movie_data[0], movie_data[4])
+    return flask.render_template(
+        "index.html",
+        data=movie_data,
+        link=wiki_link
+        )
+
+app.run()
