8f70c49e8131f95d453fb65f835312fce5a4d613
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-09 19:18:26 -0500

    added movies to display on refresh and took out extra pages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask
 from tmdb import get_movie_data
+import random
 
 app = flask.Flask(__name__)
 """routes interpret different pages of a page"""
@@ -7,37 +8,24 @@ app = flask.Flask(__name__)
 @app.route("/")
 def index():
     """home page"""
-    return flask.render_template("index.html")
+    external_ids = ["558", "557", "419430", "634649"]
+    external_id = random.choice(external_ids)
+    movie_data = get_movie_data(external_id)
+    return flask.render_template(
+        "index.html",
+        data=movie_data,
+        )
     #return "Hello World"
 
 @app.route("/movie_one.html")
 def movie_one():
     """movie_one page"""
-    external_id = "157336"
+    external_ids = ["558", "557", "419430", "634649"]
+    external_id = random.choice(external_ids)
     movie_data = get_movie_data(external_id)
     return flask.render_template(
         "movie_one.html",
         data=movie_data,
         )
-
-@app.route("/movie_two.html")
-def movie_two():
-    """movie_two page"""
-    return flask.render_template("movie_two.html")
-
-@app.route("/movie_three.html")
-def movie_three():
-    """movie_three page"""
-    return flask.render_template("movie_three.html")
-
-@app.route("/movie_four.html")
-def movie_four():
-    """movie_four page"""
-    return flask.render_template("movie_four.html")
-
-@app.route("/movie_five.html")
-def movie_five():
-    """movie_five page"""
-    return flask.render_template("movie_five.html")
 
 app.run()
