a824ee54b0155e5a02e6e9f755b3c227ef56b5bc
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-09 22:29:50 -0500

    got rid of extra html file because I don't need it and added wikipedia functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-from tmdb import get_movie_data
+from tmdb import get_movie_data, wiki_page
 import random
 
 app = flask.Flask(__name__)
@@ -11,21 +11,11 @@ def index():
     external_ids = ["558", "557", "419430", "634649"]
     external_id = random.choice(external_ids)
     movie_data = get_movie_data(external_id)
+    wiki_link = wiki_page(movie_data[0], movie_data[4])
     return flask.render_template(
         "index.html",
         data=movie_data,
-        )
-    #return "Hello World"
-
-@app.route("/movie_one.html")
-def movie_one():
-    """movie_one page"""
-    external_ids = ["558", "557", "419430", "634649"]
-    external_id = random.choice(external_ids)
-    movie_data = get_movie_data(external_id)
-    return flask.render_template(
-        "movie_one.html",
-        data=movie_data,
+        link=wiki_link
         )
 
 app.run()
