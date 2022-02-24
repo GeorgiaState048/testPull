2c640531d24b790cf446ecb241d7b0a7048ec8b4
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-24 15:48:42 -0500

    initial commit

--- app.py
+++ app.py
@@ -7,22 +7,24 @@ from tmdb_wiki import get_movie_data, wiki_page
 
 app = flask.Flask(__name__)
 """routes interpret different pages of a page"""
-
+three = set()
 @app.route("/")
 def index():
     """home page"""
     external_ids = ["27205", "557", "419430", "496243"]
-    external_id = random.choice(external_ids)
+    if len(three) == 3:
+        threes = list(three)
+        external_id = random.choice(threes)
+    else:
+        external_id = random.choice(external_ids)
+        three.add(external_id)
+    print(three)
     movie_data = get_movie_data(external_id)
     wiki_link = wiki_page(movie_data[0], movie_data[4])
     return flask.render_template(
-        "index.html",
+        "test.html",
         data=movie_data,
         link=wiki_link
         )
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+app.run()
