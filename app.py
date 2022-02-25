cae2a4fbc7ee37497b6cdc04c967fa75d81c72be
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-25 02:26:21 -0500

    initial commit

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ import os
 import random
 import flask
 from tmdb_wiki import get_movie_data, wiki_page
+from flask_sqlalchemy import SQLAlchemy
 
+app = flask.Flask(__name__)
+
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pnjonadutwpcns:68c0e33ca6cea3a2c70487bf5694f03e185232d4636ce2c57844651ca3b180ce@ec2-44-192-245-97.compute-1.amazonaws.com:5432/ddggelsgi946kc"
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 app = flask.Flask(__name__)
 """routes interpret different pages of a page"""
@@ -12,19 +21,13 @@ three = set()
 def index():
     """home page"""
     external_ids = ["27205", "557", "419430", "496243"]
-    if len(three) == 3:
-        threes = list(three)
-        external_id = random.choice(threes)
-    else:
-        external_id = random.choice(external_ids)
-        three.add(external_id)
-    print(three)
+    external_id = random.choice(external_ids)
     movie_data = get_movie_data(external_id)
     wiki_link = wiki_page(movie_data[0], movie_data[4])
     return flask.render_template(
-        "test.html",
+        "index.html",
         data=movie_data,
         link=wiki_link
         )
 
-app.run()
+app.run(debug=True)
