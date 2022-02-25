07347530a7e11a18ac31ad12f409619eb61f0c0f
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-02-25 18:24:01 -0500

    commit

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-import psycopg2
 from wtforms.validators import InputRequired, Length, ValidationError
 
 app = flask.Flask(__name__)
